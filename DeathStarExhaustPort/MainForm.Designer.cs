4f454c8f992865e9cd72b679c147810f0e4b7f07
Jamal S <jamasouk354@ed.amdsb.ca>
2018-11-30 14:54:49 -0500

    Add project files.

---
+++ DeathStarExhaustPort/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DeathStarExhaustPort
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.fullButton = new System.Windows.Forms.Button();
+            this.partButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fullButton
+            // 
+            this.fullButton.Location = new System.Drawing.Point(654, 674);
+            this.fullButton.Name = "fullButton";
+            this.fullButton.Size = new System.Drawing.Size(75, 40);
+            this.fullButton.TabIndex = 0;
+            this.fullButton.Text = "Full";
+            this.fullButton.UseVisualStyleBackColor = true;
+            this.fullButton.Click += new System.EventHandler(this.fullButton_Click);
+            // 
+            // partButton
+            // 
+            this.partButton.Location = new System.Drawing.Point(654, 619);
+            this.partButton.Name = "partButton";
+            this.partButton.Size = new System.Drawing.Size(75, 40);
+            this.partButton.TabIndex = 1;
+            this.partButton.Text = "Part";
+            this.partButton.UseVisualStyleBackColor = true;
+            this.partButton.Click += new System.EventHandler(this.partButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::DeathStarExhaustPort.Properties.Resources.RebelSymbol;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(741, 726);
+            this.Controls.Add(this.partButton);
+            this.Controls.Add(this.fullButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Death Star Attack Plan";
+            this.Click += new System.EventHandler(this.MainForm_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fullButton;
+        private System.Windows.Forms.Button partButton;
+    }
+}
+
---
+++ DeathStarExhaustPort/MainForm.cs
@@ -0,0 +1,195 @@
+﻿/// Created by Mr. T. 
+/// August 2017
+/// 
+/// This program is used as a template to test the draw methods that each student will
+/// create and then combine into one group project. 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Media;
+
+namespace DeathStarExhaustPort
+{
+    public partial class MainForm : Form
+    {
+        Graphics onScreen;
+
+        Bitmap bm; //bitmap area size of whole screen
+        Graphics offScreen; //Sets off-screen graphics to the bitmap
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            onScreen = this.CreateGraphics();
+            bm = new Bitmap(this.Width, this.Height); //bitmap area size of whole screen
+            offScreen = Graphics.FromImage(bm); //Sets off-screen graphics to the bitmap
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player;
+
+            int shipX = 360;
+            int shipY = 25;
+
+            int torpedoX = 265;
+            int torpedoY = 35;
+
+            // ************************** X wing fly in **************************
+            for (int x = 0; x < 10; x++)
+            {
+                shipX = shipX - 10;
+
+                Thread.Sleep(50);
+                offScreen.Clear(Color.Black);              
+
+                DeathStar(55, 55, 400);
+
+                ExhaustPort(245, 62, 20, 205);
+                Xwing(shipX, shipY, 30, 8);
+
+                onScreen.DrawImage(bm, 0, 0);
+            }
+
+            // ************************** X - wing fly out and torpedo fly in  **************************
+            player = new SoundPlayer(Properties.Resources.torpedo);
+            player.Play();
+
+            for (int x = 0; x < 4; x++)
+            {
+                shipX -= 8;
+                shipY -= 9;
+
+                torpedoX -= 5;
+                torpedoY += 5;
+
+                Thread.Sleep(50);
+                offScreen.Clear(Color.Black);
+                
+                DeathStar(55, 55, 400);
+                ExhaustPort(245, 62, 20, 205);
+                Xwing(shipX, shipY, 30, 10);
+                Torpedo(torpedoX, torpedoY, 20);
+
+                onScreen.DrawImage(bm, 0, 0);
+            }
+
+            // ************************** torpedo drop **************************
+            for (int x = 0; x < 38; x++)
+            {
+                torpedoY += 5;
+
+                Thread.Sleep(50);
+                offScreen.Clear(Color.Black);
+                
+                DeathStar(55, 55, 400);
+                ExhaustPort(245, 62, 20, 205);
+                Xwing(shipX, shipY, 30, 8);
+                Torpedo(torpedoX, torpedoY, 20);
+
+                onScreen.DrawImage(bm, 0, 0);
+            }
+
+            // ************************** Explosion **************************
+            player = new SoundPlayer(Properties.Resources.explosion);
+            player.Play();
+
+            for (int x = 1; x < 10; x++)
+            {
+                Thread.Sleep(150);
+                offScreen.Clear(Color.Black);             
+                
+                DeathStar(55, 55, 400);
+                ExhaustPort(245, 62, 20, 205);               
+
+                if (x % 2 == 0) { Explosion(205, 205, 100); }
+                else            { Explosion(155, 155, 200); }
+
+                onScreen.DrawImage(bm, 0, 0);
+            }
+        }
+
+        public void Xwing(float x, float y, float width, float height)
+        {
+            Pen shipPen = new Pen(Color.White);
+
+            /// Use the rectangle below for testing purposes. 
+            /// Your shape should always draw within this rectangle, reglardless of size and position.
+            /// Comment it out when you are done.
+            offScreen.DrawRectangle(shipPen, x, y, width, height);
+
+        }
+
+        public void Torpedo(float x, float y, float pixels)
+        {
+            Pen torpPen = new Pen(Color.White);
+
+            /// Use the rectangle below for testing purposes. 
+            /// Your shape should always draw within this rectangle, reglardless of size and position.
+            /// Comment it out when you are done.
+            offScreen.DrawRectangle(torpPen, x, y, pixels, pixels);
+        }
+
+        public void Explosion(float x, float y, float pixels)
+        {
+            Pen exPen = new Pen(Color.White);
+
+            /// Use the rectangle below for testing purposes. 
+            /// Your shape should always draw within this rectangle, reglardless of size and position.
+            /// Comment it out when you are done.
+            offScreen.DrawRectangle(exPen, x, y, pixels, pixels);           
+        }
+
+        public void DeathStar(float x, float y, float pixels)
+        {
+            Pen deathPen = new Pen(Color.White);
+
+            /// Use the rectangle below for testing purposes. 
+            /// Your shape should always draw within this rectangle, reglardless of size and position.
+            /// Comment it out when you are done.
+            offScreen.DrawEllipse(deathPen, x, y, pixels, pixels);
+        }
+
+        public void ExhaustPort(float x, float y, float width, float height)
+        {
+            Pen exPen = new Pen(Color.White);
+            Pen rPen = new Pen(Color.Red);
+
+            /// Use the rectangle below for testing purposes. 
+            /// Your shape should always draw within this rectangle, reglardless of size and position.
+            /// Comment it out when you are done.
+            offScreen.DrawRectangle(exPen, x, y, width, height); //Dimensions 20Wx205H
+
+            offScreen.DrawLine(rPen, 7, 0, 7, height-20);
+            offScreen.DrawLine(rPen, 13, 0, 13, height-20);
+            offScreen.DrawArc(exPen, 0, height-20, 20, 20, 135, 330);
+        }
+
+        private void fullButton_Click(object sender, EventArgs e)
+        {
+            MainForm_Click(sender, e);
+        }
+
+        private void partButton_Click(object sender, EventArgs e)
+        {
+            offScreen.Clear(Color.Black); // do not remove
+
+            /// Call your method here. This is where you can adjust the location and size 
+            /// to make sure that it draws on the screen correctly.
+            ExhaustPort(0, 0, 20, 205);
+
+
+            // Draws to the screen 
+            onScreen.DrawImage(bm, 0, 0);
+        }
+    }
+}
